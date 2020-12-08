aa10b77f29e9ca7cb484fb8baba62a5b3230c50c
arfaoui-aziz <aziz.arfaou.98@gmail.com>
2020-12-08 11:15:32 +0100

    Workshop V0 UI

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { FiltersComponent } from './shopping-cart/filters/filters.component';
+import { ProductListComponent } from './shopping-cart/product-list/product-list.component';
+import { CartComponent } from './shopping-cart/cart/cart.component';
+import { CartItemComponent } from './shopping-cart/cart/cart-item/cart-item.component';
+import { ProductItemComponent } from './shopping-cart/product-list/product-item/product-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ShoppingCartComponent,
+    FiltersComponent,
+    ProductListComponent,
+    CartComponent,
+    CartItemComponent,
+    ProductItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
